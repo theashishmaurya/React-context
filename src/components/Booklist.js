e3d1f73df116d75d5c42cdac328cf91ab041814d
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-07-28 00:15:41 +0530

    commit

---
+++ src/components/Booklist.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Booklist extends Component {
+    render() {
+        return (
+            <div className = 'book-list'>
+            <ul>
+                <li>the way of kings</li>
+                <li>this is a book</li>
+                <li>this is a book</li>
+            </ul>
+                
+            </div>
+        )
+    }
+}
+
+export default Booklist
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar
